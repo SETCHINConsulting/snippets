704c7880a87e23108ea798101af94f996f843c8a
Vladislav Setchin <vlad@setchinfc.com.au>
2019-09-08 11:54:22 +1000

    Added Command Line Args snippet project

---
+++ CommandLineArgsVS/CommandLineArgs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Configuration.CommandLine;
+
+namespace CommandLineArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new CommandLineConfigurationProvider(args);
+            c.Load();
+            string serverName = string.Empty;
+            c.TryGet("server", out serverName);
+
+            string username = string.Empty;
+            c.TryGet("username", out username);
+
+            string database = string.Empty;
+            c.TryGet("database", out database);
+        }
+    }
+}
